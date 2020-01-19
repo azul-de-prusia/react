02aa0c01b9fca419ac8e01dc99bb15084f01e70d
azul-de-prusia <tegthyn_600@hotmail.com>
2020-01-18 19:08:43 -0600

    inputs update and iteration with map method

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
 //impotación de la librería React y la clase component
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 //Esqueleto del componente
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props)
+    //estado:
+    this.state = {
+       newTask: "",
+       tasks: []
+    };
+ }
+ 
+  handleTaskChange = (event) => {
+    this.setState({
+     newTask: event.target.value,
+    })
+  }
+
+  //preventDefault evita el refresh, se guarda una copia de las tareas en newTasks, luego empuja el contenido del input dentro del arreglo y deja el input limpio para no duplicar]
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let newTasks = this.state.tasks;
+    newTasks.push(this.state.newTask)
+    this.setState({
+    newTask: "",
+    tasks: newTasks
+  })
+}
+
+ render() {
   //Retorna un componente escrito en JSX
   return (
-    //Componente
     <div className="container">
-      <h1 className="title">
-        Hello world <span aria-label="emoji" role="img">🔥</span>
-      </h1>
-      <input type="text" className="new-task"/>
+       <h1 className="title">To do list<span aria-label="emoji" role="img">🔥</span></h1>
+       [onChangecambia el valor del estado en el input, onSubmit hace que se recargue porque debería enviar los datos a un servidor, preventDefault evita el refresh]
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.newTask} 
+            onChange={this.handleTaskChange}
+            type="text" 
+            className="new-task" 
+          />
+        </form>
+       <h2 className="test-label">{this.state.newTask}</h2>
+
+[este pedacito usa .map de JS para iterar task y almacena en un h3 con lo que podremos después mostrar el contenido]
+      {
+        this.state.tasks.map(task =>
+        <div className="task-container">
+        <h3 className="task">{task}</h3>
+        </div>
+        )
+      }
     </div>
-  );
+ );
+}
 }
 
 //Exportación del componente principal para que pueda ser importado desde index.js
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+//Archivos de importación desde node modules o desde el directorio indicado
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//Render de ReactDOM es la función que recibe 2 parámetros; qué voy a renderizar? y dónde?
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
